1aaf6a23158fbe4f049efcdd2b7b3d2426073fc3
Den <lukashov.d.n@gmail.com>
2015-09-06 16:39:48 +0300

    Add "for i=0; < 8" to setupViewPager

--- app/src/main/java/com/example/d_rom/supportdesigndemo/MainActivity.java
+++ app/src/main/java/com/example/d_rom/supportdesigndemo/MainActivity.java
@@ -93,13 +93,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void setupViewPager(ViewPager _viewPager) {
         TabFragmentAdapter adapter = new TabFragmentAdapter(getSupportFragmentManager());
-        adapter.addFragment(new SimpleListFragment(), "Tab 1");
-        adapter.addFragment(new SimpleListFragment(), "Tab 2");
-        adapter.addFragment(new SimpleListFragment(), "Tab 3");
-        adapter.addFragment(new SimpleListFragment(), "Tab 4");
-        adapter.addFragment(new SimpleListFragment(), "Tab 5");
-        adapter.addFragment(new SimpleListFragment(), "Tab 6");
-        adapter.addFragment(new SimpleListFragment(), "Tab 7");
+
+        for (int i = 0; i < 8 ; i++) {
+            adapter.addFragment(new SimpleListFragment(), "Tab " + i);
+        }
         _viewPager.setAdapter(adapter);
     }
 
